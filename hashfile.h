7bee89826d4786b2940fdfb634df1bb3b98ab3ab
The1Dali <247246441+The1Dali@users.noreply.github.com>
2026-03-17 14:11:41 +0100

    hashfile.h Implementation

---
+++ hashfile.h
@@ -0,0 +1,16 @@
+#ifndef HASHFILE_H
+#define HASHFILE_H
+
+#include <stddef.h>  
+
+typedef struct {
+    char  hash_hex[129];    
+    char  username[256];     
+    char  plaintext[256];   
+    int   cracked;          
+} Target;
+
+
+int hashfile_load(const char *path, Target **targets, size_t *count);
+
+#endif
