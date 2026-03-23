37e27f9478f3fabfd762bedffb68e7722b767303
The1Dali <247246441+The1Dali@users.noreply.github.com>
2026-03-23 22:08:47 +0100

    Updated hash.h

--- hash.h
+++ hash.h
@@ -4,7 +4,9 @@
 #include "config.h"
 #include <stddef.h>
 
+
 typedef void (*HashFn)(const unsigned char *, size_t, unsigned char *);
+
 
 typedef struct
 {
@@ -13,6 +15,14 @@ typedef struct
     HashFn      fn;
 } HashDef;
 
+
 void hash_compute(HashAlgo algo, const char *input, size_t len, char *out_hex);
+
+
+size_t hash_compute_raw(HashAlgo algo, const char *input, size_t len,
+                        unsigned char *out_raw);
+
+
+size_t hex_to_bytes(const char *hex, unsigned char *out);
 
 #endif
