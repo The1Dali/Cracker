4db2b12eac1e7f1abfd964388a0c37130394526d
The1Dali <247246441+The1Dali@users.noreply.github.com>
2026-03-23 12:21:40 +0100

    Improved hash.h

--- hash.h
+++ hash.h
@@ -2,8 +2,16 @@
 #define HASH_H
 
 #include "config.h"
-#include <stddef.h>   
+#include <stddef.h>
 
+typedef void (*HashFn)(const unsigned char *, size_t, unsigned char *);
+
+typedef struct
+{
+    const char *name;
+    size_t      digest_len;
+    HashFn      fn;
+} HashDef;
 
 void hash_compute(HashAlgo algo, const char *input, size_t len, char *out_hex);
 
