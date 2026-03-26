b34864e918b35f107b3023e939a3f3177467eadc
The1Dali <247246441+The1Dali@users.noreply.github.com>
2026-03-26 17:09:44 +0100

    Brute Force Config

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
-#ifndef CONFIG_H   
+#ifndef CONFIG_H
 #define CONFIG_H
-typedef enum 
+
+typedef enum
 {
     HASH_MD5    = 0,
     HASH_SHA256 = 1,
@@ -8,12 +9,31 @@ typedef enum
     HASH_NTLM   = 3,
 } HashAlgo;
 
-typedef struct {
-    HashAlgo    algo;           
-    char        hashfile[512];  
-    char        wordlist[512];  
-    char        outfile[512];  
-    int         verbose;      
+typedef enum
+{
+    ATTACK_DICTIONARY  = 0,
+    ATTACK_BRUTEFORCE  = 1,
+} AttackMode;
+
+#define CHARSET_LOWER   "abcdefghijklmnopqrstuvwxyz"
+#define CHARSET_UPPER   "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define CHARSET_DIGITS  "0123456789"
+#define CHARSET_ALNUM   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+#define CHARSET_ALL     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*"
+
+#define MAX_BF_LEN 16
+
+typedef struct
+{
+    HashAlgo    algo;
+    AttackMode  mode;
+    char        hashfile[512];
+    char        wordlist[512];
+    char        charset[128];
+    char        outfile[512];
+    int         min_len;
+    int         max_len;
+    int         verbose;
 } Config;
 
-#endif 
+#endif
