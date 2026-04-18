6abdac87edc5cab624bec15f3effc4b4b09e9222
ala02tn <ala02tn@gmail.com>
2026-04-18 20:29:17 +0100

    Final hashfile.h

--- hashfile.h
+++ hashfile.h
@@ -2,19 +2,20 @@
 #define HASHFILE_H
 
 #include <stddef.h>
-
-
 typedef struct
 {
     char          hash_hex[129];
     unsigned char digest[64];
     size_t        digest_len;
+    char          salt[64];
+    size_t        salt_len;
+    int           has_salt;
     char          username[256];
     char          plaintext[256];
     int           cracked;
 } Target;
 
-
-int hashfile_load(const char *path, Target **targets, size_t *count);
+int hashfile_load(const char *path, Target **targets, size_t *count,
+                  size_t *n_unsalted);
 
 #endif
